ba68449e8fc6f7549edd5c9a62c04b2704458ba9
Sarmix <wdsarmientom@libertadores.edu.co>
2023-10-25 22:44:09 -0500

    f

--- app/src/main/java/com/example/interfaz_parcial/Inicio.java
+++ app/src/main/java/com/example/interfaz_parcial/Inicio.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 public class Inicio extends AppCompatActivity {
     Button agregar;
     Button eliminar;
+    Button editar;
+    Button Listar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +24,30 @@ public class Inicio extends AppCompatActivity {
                 Intent i = new Intent(Inicio.this, Agregar.class);
                 startActivity(i);
             }
-
-
-
         });
-        agregar=(Button)findViewById(R.id.botoneliminar);
-        agregar.setOnClickListener(new View.OnClickListener(){
+        eliminar=(Button)findViewById(R.id.botoneliminar);
+        eliminar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Inicio.this, Eliminar.class);
                 startActivity(i);
             }
-
-
-
+        });
+        editar=(Button)findViewById(R.id.botoneditar);
+        editar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Inicio.this, Editar.class);
+                startActivity(i);
+            }
+        });
+        Listar=(Button)findViewById(R.id.botonlistar);
+        Listar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Inicio.this, Listar.class);
+                startActivity(i);
+            }
         });
     }
 }
