babd1eddac2c2456e14625029f43ef4c43f6fa76
Sarmix <wdsarmientom@libertadores.edu.co>
2023-10-25 21:44:49 -0500

    f

--- app/src/main/java/com/example/interfaz_parcial/Agregar.java
+++ app/src/main/java/com/example/interfaz_parcial/Agregar.java
@@ -3,12 +3,26 @@ package com.example.interfaz_parcial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Agregar extends AppCompatActivity {
+
+    Button agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar);
+        agregar=(Button)findViewById(R.id.Botonagregar);
+        agregar.setOnClickListener( new View.OnClickListener(){
+            Intent i = new Intent(Inicio.this, Agregar.class);
+
+
+        });
     }
 }
